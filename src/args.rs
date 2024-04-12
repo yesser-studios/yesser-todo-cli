495b3c313e9cad724906c394da157e044828a2bb
yesseruser <yesseruseryt@gmail.com>
2024-04-12 23:53:36 +0200

    Improve and finalise args

--- src/args.rs
+++ src/args.rs
@@ -7,11 +7,40 @@ use clap:: {
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub(crate) struct TodoArgs {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    pub(crate) name: String,
+    /// The operation to do in the task list.
+    #[clap(subcommand)]
+    pub(crate) command: Command
+}
 
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    pub(crate) count: u8,
+#[derive(Debug, Subcommand)]
+pub(crate) enum Command {
+    /// Add tasks to the task list. Separate tasks with spaces.
+    Add(AddCommand),
+    /// Remove tasks from the task list. Separate tasks with spaces.
+    Remove(RemoveCommand),
+    /// Mark tasks in the task list as done.
+    Done(DoneCommand),
+    /// List all tasks. Tasks marked done are shown with a strike-through.
+    List
+}
+
+#[derive(Debug, Args)]
+pub(crate) struct AddCommand {
+    /// The tasks to add
+    #[arg(num_args = 0..)]
+    pub tasks: Option<Vec<String>>
+}
+
+#[derive(Debug, Args)]
+pub(crate) struct RemoveCommand {
+    /// The tasks to remove
+    #[arg(num_args = 0..)]
+    pub tasks: Option<Vec<String>>
+}
+
+#[derive(Debug, Args)]
+pub(crate) struct DoneCommand {
+    /// The tasks to mark done
+    #[arg(num_args = 0..)]
+    pub tasks: Option<Vec<String>>
 }
