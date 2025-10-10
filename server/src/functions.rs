70f024d97d2af60d4f2431c6ac6980d3e629d938
yesseruser <yesseruseryt@gmail.com>
2025-10-10 18:18:12 +0200

    Move API functions to functions.rs

---
+++ server/src/functions.rs
@@ -0,0 +1,18 @@
+use axum::{debug_handler, Json};
+use yesser_todo_db::{SaveData, Task};
+
+#[debug_handler]
+pub async fn get_tasks() -> Json<Vec<Task>> {
+    let mut save_data = SaveData::new();
+    let _ = save_data.load_tasks();
+    let tasks = save_data.get_tasks().clone();
+    Json(tasks)
+}
+
+#[debug_handler]
+pub async fn add_task() {
+    let mut save_data = SaveData::new();
+    let _ = save_data.load_tasks();
+    save_data.add_task(Task{name: String::from("Task"), done: false});
+    save_data.save_tasks().unwrap();
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,7 @@
+mod functions;
+
 use axum::{debug_handler, routing::{get, post}, Json, Router};
-use yesser_todo_db::{SaveData, Task};
+use crate::functions::{add_task, get_tasks};
 
 #[tokio::main]
 async fn main() {
@@ -9,20 +11,4 @@ async fn main() {
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:6982").await.unwrap();
     axum::serve(listener, router).await.unwrap();
-}
-
-#[debug_handler]
-async fn get_tasks() -> Json<Vec<Task>> {
-    let mut save_data = SaveData::new();
-    let _ = save_data.load_tasks();
-    let tasks = save_data.get_tasks().clone();
-    Json(tasks)
-}
-
-#[debug_handler]
-async fn add_task() {
-    let mut save_data = SaveData::new();
-    let _ = save_data.load_tasks();
-    save_data.add_task(Task{name: String::from("Task"), done: false});
-    save_data.save_tasks().unwrap();
 }
