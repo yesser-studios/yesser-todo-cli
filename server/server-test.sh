85d9b222d379a96ff2c02ffc86e8e5b5cd5311ae
yesseruser <yesseruseryt@gmail.com>
2025-10-10 18:34:57 +0200

    Add index to remove in test

--- server/server-test.sh
+++ server/server-test.sh
@@ -2,4 +2,4 @@
 
 curl http://127.0.0.1:6982/add -X POST --json '"hello"'
 curl http://127.0.0.1:6982/tasks
-curl http://127.0.0.1:6982/remove -X DELETE
+curl http://127.0.0.1:6982/remove -X DELETE --json '0'
