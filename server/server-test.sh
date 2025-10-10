4a1e68b6ec08aa277cb3c44a6408ff6ee3406967
yesseruser <yesseruseryt@gmail.com>
2025-10-10 18:55:12 +0200

    Add newlines in test

--- server/server-test.sh
+++ server/server-test.sh
@@ -5,9 +5,11 @@ curl http://127.0.0.1:6982/tasks
 curl http://127.0.0.1:6982/done -X POST --json '0'
 curl http://127.0.0.1:6982/undone -X POST --json '0'
 curl http://127.0.0.1:6982/remove -X DELETE --json '0'
+printf "\n"
 curl http://127.0.0.1:6982/add -X POST --json '"hello-clear"'
 curl http://127.0.0.1:6982/clear -X DELETE
 curl http://127.0.0.1:6982/tasks
+printf "\n"
 curl http://127.0.0.1:6982/add -X POST --json '"hello-clear-done"'
 curl http://127.0.0.1:6982/done -X POST --json '0'
 curl http://127.0.0.1:6982/cleardone -X DELETE
