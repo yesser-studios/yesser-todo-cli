623e5c9edcc3a69be337cf4737770dfe7f718817
yesseruser <yesseruseryt@gmail.com>
2025-10-10 19:26:22 +0200

    Add client struct and get function to api

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,14 +1,36 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use reqwest::{Error, Response};
+use yesser_todo_db::Task;
+
+pub struct Client {
+    pub hostname: String,
+    pub port: String,
+    client: reqwest::Client,
+}
+
+impl Client {
+    pub fn new(hostname: String, port: String) -> Client {
+        return Client{hostname, port, client: reqwest::Client::new(), };
+    }
+
+    async fn get(&self) -> Result<Vec<Task>, Error> {
+        let result = self.client
+            .get(format!("https://{}:{}/tasks", self.hostname, self.port).as_str())
+            .send().await;
+
+        match result {
+            Ok(result) => {
+                let result = result.json::<Vec<Task>>().await;
+                match result {
+                    Ok(result) => {Ok(result)},
+                    Err(err) => {Err(err)}
+                }
+            }
+            Err(err) => {Err(err)}
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
