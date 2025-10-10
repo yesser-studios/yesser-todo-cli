23eeff2bc3a5c2110025c81a2f02a1278eff9ae0
yesseruser <yesseruseryt@gmail.com>
2025-10-10 17:19:30 +0200

    Add add_task

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,7 +4,8 @@ use yesser_todo_db::{SaveData, Task};
 #[tokio::main]
 async fn main() {
     let router = Router::new()
-        .route("/tasks", get(get_tasks));
+        .route("/tasks", get(get_tasks))
+        .route("/add", post(add_task));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:6982").await.unwrap();
     axum::serve(listener, router).await.unwrap();
@@ -15,4 +16,11 @@ async fn get_tasks() -> Json<Vec<Task>> {
     let _ = save_data.load_tasks();
     let tasks = save_data.get_tasks().clone();
     Json(tasks)
+}
+
+async fn add_task() {
+    let mut save_data = SaveData::new();
+    let _ = save_data.load_tasks();
+    save_data.add_task(Task{name: String::from("Task"), done: false});
+    save_data.save_tasks().unwrap();
 }
