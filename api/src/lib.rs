a82ac783437bf1977c48fca2e7476a777a66ab04
yesseruser <yesseruseryt@gmail.com>
2025-10-10 19:51:30 +0200

    Add test and fix issues

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,3 +1,4 @@
+use std::string::ToString;
 use reqwest::{Error, Response};
 use yesser_todo_db::Task;
 
@@ -8,13 +9,16 @@ pub struct Client {
 }
 
 impl Client {
-    pub fn new(hostname: String, port: String) -> Client {
-        return Client{hostname, port, client: reqwest::Client::new(), };
+    pub fn new(hostname: String, port: Option<String>) -> Client {
+        match port {
+            None => {Client{hostname, port: "6982".to_string(), client: reqwest::Client::new(), }}
+            Some(port) => {Client{hostname, port, client: reqwest::Client::new(), }}
+        }
     }
 
     async fn get(&self) -> Result<Vec<Task>, Error> {
         let result = self.client
-            .get(format!("https://{}:{}/tasks", self.hostname, self.port).as_str())
+            .get(format!("{}:{}/tasks", self.hostname, self.port).as_str())
             .send().await;
 
         match result {
@@ -31,7 +35,7 @@ impl Client {
 
     async fn add(&self, task: Task) -> Result<Task, Error> {
         let result = self.client
-            .post(format!("https://{}:{}/add", self.hostname, self.port).as_str())
+            .post(format!("{}:{}/add", self.hostname, self.port).as_str())
             .json(&task)
             .send().await;
 
@@ -51,4 +55,12 @@ impl Client {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[tokio::test]
+    async fn get() {
+        let client = Client::new("http://127.0.0.1".to_string(), None);
+        let result = client.get().await;
+        println!("{:?}", result);
+        assert!(result.is_ok())
+    }
 }
