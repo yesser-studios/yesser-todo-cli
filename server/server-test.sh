9368916e5b69a4311c23006623e94cc9b7b82324
yesseruser <yesseruseryt@gmail.com>
2025-10-10 18:54:03 +0200

    Add tests for clear and clear_done functions

--- server/server-test.sh
+++ server/server-test.sh
@@ -5,3 +5,10 @@ curl http://127.0.0.1:6982/tasks
 curl http://127.0.0.1:6982/done -X POST --json '0'
 curl http://127.0.0.1:6982/undone -X POST --json '0'
 curl http://127.0.0.1:6982/remove -X DELETE --json '0'
+curl http://127.0.0.1:6982/add -X POST --json '"hello-clear"'
+curl http://127.0.0.1:6982/clear -X DELETE
+curl http://127.0.0.1:6982/tasks
+curl http://127.0.0.1:6982/add -X POST --json '"hello-clear-done"'
+curl http://127.0.0.1:6982/done -X POST --json '0'
+curl http://127.0.0.1:6982/cleardone -X DELETE
+curl http://127.0.0.1:6982/tasks
