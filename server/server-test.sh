751246c12caac66a37bd381adb6bfd6dc2d3005f
yesseruser <yesseruseryt@gmail.com>
2025-10-10 18:47:47 +0200

    Test done and undone functions

--- server/server-test.sh
+++ server/server-test.sh
@@ -2,4 +2,6 @@
 
 curl http://127.0.0.1:6982/add -X POST --json '"hello"'
 curl http://127.0.0.1:6982/tasks
+curl http://127.0.0.1:6982/done -X POST --json '0'
+curl http://127.0.0.1:6982/undone -X POST --json '0'
 curl http://127.0.0.1:6982/remove -X DELETE --json '0'
