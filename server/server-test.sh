00c2fa149fb4fd4c839ccdaf57784b763284ea92
yesseruser <yesseruseryt@gmail.com>
2025-10-10 18:31:18 +0200

    Specify json for add in test

--- server/server-test.sh
+++ server/server-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl http://127.0.0.1:6982/add -X POST
+curl http://127.0.0.1:6982/add -X POST --json '"hello"'
 curl http://127.0.0.1:6982/tasks
 curl http://127.0.0.1:6982/remove -X DELETE
