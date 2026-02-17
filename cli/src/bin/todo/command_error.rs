00c67229a2c17fcc7dfce044932e5cec4eb8d167
yesseruser <yesseruseryt@gmail.com>
2026-02-17 23:31:06 +0100

    Add handle function to CommandError

--- cli/src/bin/todo/command_error.rs
+++ cli/src/bin/todo/command_error.rs
@@ -4,3 +4,22 @@ pub(crate) enum CommandError {
     HTTPError { name: String, status_code: u16 },
     ConnectionError { name: String },
 }
+
+impl CommandError {
+    pub(crate) fn handle(&self) {
+        match self {
+            CommandError::TaskExists { name } => {
+                eprintln!("Task {name} already exists!")
+            }
+            CommandError::TaskNotFound { name } => {
+                eprintln!("Task {name} not found!")
+            }
+            CommandError::HTTPError { name, status_code } => {
+                eprintln!("HTTP error code {status_code} for task {name}!")
+            }
+            CommandError::ConnectionError { name } => {
+                eprintln!("Failed to connect to the server for task {name}!")
+            }
+        }
+    }
+}
