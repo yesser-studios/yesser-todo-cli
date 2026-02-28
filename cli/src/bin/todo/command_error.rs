a665de2afe50ca85c772c94b68bed9263e43b58e
yesseruser <yesseruseryt@gmail.com>
2026-02-28 03:47:06 +0100

    Implement Display for CommandError

--- cli/src/bin/todo/command_error.rs
+++ cli/src/bin/todo/command_error.rs
@@ -1,3 +1,8 @@
+use std::fmt::{self, Display, Formatter};
+
+use thiserror::Error;
+
+#[derive(Debug, Error)]
 pub(crate) enum CommandError {
     NoTasksSpecified,
     TaskExists { name: String },
@@ -10,30 +15,32 @@ pub(crate) enum CommandError {
 
 impl CommandError {
     pub(crate) fn handle(&self) {
+        eprintln!("{self}")
+    }
+}
+
+impl Display for CommandError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self {
-            CommandError::NoTasksSpecified => {
-                println!("No tasks specified!") // TODO: Change to eprintln! in next major version
+            CommandError::NoTasksSpecified => write!(f, "No tasks specified!"),
+            CommandError::TaskExists { name } => write!(f, "Task {name} already exists!"),
+            CommandError::TaskNotFound { name } => write!(f, "Task {name} not found!"),
+            CommandError::DataError { what } => write!(f, "Unable to save {what}!"),
+            CommandError::HTTPError { name, status_code } => {
+                write!(
+                    f,
+                    "HTTP error code {status_code}{}!",
+                    if name.is_empty() { "".into() } else { format!(" for task {name}") }
+                )
             }
-            CommandError::TaskExists { name } => {
-                println!("Task {name} already exists!")
+            CommandError::ConnectionError { name } => {
+                write!(
+                    f,
+                    "Failed to connect to the server{}!",
+                    if name.is_empty() { "".into() } else { format!(" for task {name}") }
+                )
             }
-            CommandError::TaskNotFound { name } => {
-                println!("Task {name} not found!")
-            }
-            CommandError::DataError { what } => {
-                println!("Unable to save {what}.")
-            }
-            CommandError::HTTPError { name, status_code } => match name.as_str() {
-                "" => println!("HTTP error code {status_code}!"),
-                _ => println!("HTTP error code {status_code} for task {name}!"),
-            },
-            CommandError::ConnectionError { name } => match name.as_str() {
-                "" => println!("Failed to connect to the server!"),
-                _ => println!("Failed to connect to the server for task {name}!"),
-            },
-            CommandError::UnlinkedError => {
-                println!("You're already unlinked!");
-            }
+            CommandError::UnlinkedError => write!(f, "You're already unlinked!"),
         }
     }
 }
