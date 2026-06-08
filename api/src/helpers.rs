f41d042025951bc62d2f95fe8a884fcbbc4de7a6
yesseruser <yesseruseryt@gmail.com>
2026-06-08 21:53:24 +0200

    Add tests for is_success
    Closes #43.

--- api/src/helpers.rs
+++ api/src/helpers.rs
@@ -1,3 +1,29 @@
 pub fn is_success(status_code: u16) -> bool {
     (200..300).contains(&status_code)
 }
+
+#[cfg(test)]
+mod test {
+    use crate::helpers::is_success;
+
+    #[test]
+    fn is_success_returns_true_for_ok() {
+        for n in 200..300 {
+            assert!(is_success(n));
+        }
+    }
+
+    #[test]
+    fn is_success_returns_false_for_input_error() {
+        for n in 400..500 {
+            assert!(!is_success(n));
+        }
+    }
+
+    #[test]
+    fn is_success_returns_false_for_server_error() {
+        for n in 500..600 {
+            assert!(!is_success(n));
+        }
+    }
+}
