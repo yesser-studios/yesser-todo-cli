800784c6b6964e44fc36e9c55d85a037593fabe5
yesseruser <yesseruseryt@gmail.com>
2026-06-19 10:40:04 +0200

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "yesser-todo-cli — A to-do CLI written in Rust";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, naersk }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk' = pkgs.callPackage naersk { };
+      in {
+        packages.default = naersk'.buildPackage {
+          src = ./.;
+          nativeBuildInputs = with pkgs; [ pkg-config ];
+          buildInputs = with pkgs; [ openssl ];
+        };
+
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ pkg-config ];
+
+          buildInputs = with pkgs; [
+            openssl
+            cargo
+            rustc
+            clippy
+            rustfmt
+            rust-analyzer
+          ];
+        };
+      });
+}
